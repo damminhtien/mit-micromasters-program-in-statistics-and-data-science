134ea290bd1de3f9e69dfdaa9e87ed9ba306cc5d
damminhtien <damminhtienchl@gmail.com>
2024-07-24 11:02:03 +0700

    complete ml prj 3

--- Machine Learning/mnist/part2-mnist/nnet_cnn.py
+++ Machine Learning/mnist/part2-mnist/nnet_cnn.py
@@ -1,17 +1,18 @@
 #! /usr/bin/env python
-
-import _pickle as c_pickle, gzip
+import sys
+sys.path.append("..")
+from train_utils import batchify_data, run_epoch, train_model, Flatten
+from utils import *
+import utils
+import _pickle as c_pickle
+import gzip
 import numpy as np
 from tqdm import tqdm
 import torch
 import torch.autograd as autograd
 import torch.nn.functional as F
 import torch.nn as nn
-import sys
-sys.path.append("..")
-import utils
-from utils import *
-from train_utils import batchify_data, run_epoch, train_model, Flatten
+
 
 def main():
     # Load the dataset
@@ -41,20 +42,37 @@ def main():
     test_batches = batchify_data(X_test, y_test, batch_size)
 
     #################################
-    ## Model specification TODO
+    # Model specification TODO
     model = nn.Sequential(
-              nn.Conv2d(1, 32, (3, 3)),
-              nn.ReLU(),
-              nn.MaxPool2d((2, 2)),
-            )
+        # Convolutional layer with 32 filters of size 3x3
+        nn.Conv2d(1, 32, kernel_size=(3, 3)),
+        nn.ReLU(),                            # ReLU nonlinearity
+        nn.MaxPool2d(kernel_size=(2, 2)),     # Max pooling layer with size 2x2
+
+        # Convolutional layer with 64 filters of size 3x3
+        nn.Conv2d(32, 64, kernel_size=(3, 3)),
+        nn.ReLU(),                            # ReLU nonlinearity
+        nn.MaxPool2d(kernel_size=(2, 2)),     # Max pooling layer with size 2x2
+
+        nn.Flatten(),                         # Flatten layer
+
+        # Fully connected layer with 128 neurons
+        nn.Linear(64 * 5 * 5, 128),
+        # nn.ReLU(),                            # ReLU nonlinearity
+        # Dropout layer with drop probability 0.5
+        nn.Dropout(p=0.5),
+
+        nn.Linear(128, 10)
+    )
     ##################################
 
     train_model(train_batches, dev_batches, model, nesterov=True)
 
-    ## Evaluate the model on test data
+    # Evaluate the model on test data
     loss, accuracy = run_epoch(test_batches, model.eval(), None)
 
-    print ("Loss on test set:"  + str(loss) + " Accuracy on test set: " + str(accuracy))
+    print("Loss on test set:" + str(loss) +
+          " Accuracy on test set: " + str(accuracy))
 
 
 if __name__ == '__main__':
